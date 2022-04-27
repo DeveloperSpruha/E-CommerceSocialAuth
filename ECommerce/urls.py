c48cc70cc9c0488f419c24ec74a7d171d9ef70ce
parseltongue13 <64133529+parseltongue13@users.noreply.github.com>
2022-04-27 18:41:12 +0530

    Payments and Post Order functionalities added

--- ECommerce/urls.py
+++ ECommerce/urls.py
@@ -22,7 +22,12 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    # Store
     path("store/", include('store.urls')),
+    # Cart
     path("cart/", include('carts.urls')),
-    path("accounts/", include('accounts.urls'))
+    # Accounts
+    path("accounts/", include('accounts.urls')),
+    # Orders
+    path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
