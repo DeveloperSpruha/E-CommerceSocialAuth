9ec19e134703ddec0c89c917dec418eb1ca7b9ef
parseltongue13 <64133529+parseltongue13@users.noreply.github.com>
2022-05-01 15:35:26 +0530

    Accounts and Order Details, Invoicing, Editing Dashboard added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,24 +1,30 @@
 from django.contrib import admin
 from django.db import OperationalError
-from .models import Account, BaseUserManager
+from .models import Account, UserProfile
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
 
 # Register your models here.
 
 class AccountAdmin(UserAdmin):
     try:
         list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
-
         readonly_fields = ('last_login', 'date_joined')
-
         ordering = ('date_joined',)
-
         list_display_links = ('email', 'first_name', 'last_name')
 
         filter_horizontal = ()
         list_filter = ()
         fieldsets = ()
+
     except OperationalError:
         pass
 
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('user', 'city', 'state', 'country')
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,15 @@
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 from email.message import EmailMessage
+from multiprocessing import context
 from typing import Type
-from django.shortcuts import render, redirect
-from .models import Account
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Account, UserProfile
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from accounts.forms import RegistrationForm
+from accounts.forms import RegistrationForm, UserProfileForm, UserForm
 import requests
+from orders.models import Order, OrderProduct
 
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
@@ -35,6 +37,11 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
 
             # User Activation
             current_site = get_current_site(request)
@@ -148,7 +155,15 @@ def activate(request, uidb64, token):
 
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    context = {
+        'orders_count': orders_count,
+        'userprofile': userprofile
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def forgotPassword(request):
@@ -207,3 +222,71 @@ def resetPassword(request):
             return redirect('resetPassword')
     else:
         return render(request, 'accounts/resetPassword.html')
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context = {
+        'orders': orders
+    }
+    return render(request, 'accounts/my_orders.html', context)
+
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == "POST":
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile has been updated.')
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'userprofile': userprofile,
+    }
+    return render(request, 'accounts/edit_profile.html', context)
+
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == "POST":
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+
+        user = Account.objects.get(username__exact=request.user.username)
+        
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                # auth.logout(request)
+                messages.success(request, "Password has updated successfully.")
+                return redirect('change_password')
+            else:
+                messages.error(request, "Please enter valid current password.")
+                return redirect('change_password')
+        else:
+            messages.error(request, "The new password and confirm password are not matching. Please try again.")
+            return redirect("change_password")
+
+    return render(request, 'accounts/change_password.html')
+
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    # The underscore underscore refers to the foreign key table value order_number in Order Model
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+    context = {
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal':  subtotal,
+    }
+    return render(request, 'accounts/order_detail.html', context)
